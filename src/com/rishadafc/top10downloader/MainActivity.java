c08eef7f3cdcd99e282aa1608cc3b4867f2b9cf2
Rishad Bharucha <rb2102@gmail.com>
2015-01-01 17:21:34 +0530

    Print XML output

--- src/com/rishadafc/top10downloader/MainActivity.java
+++ src/com/rishadafc/top10downloader/MainActivity.java
@@ -1,17 +1,31 @@
 package com.rishadafc.top10downloader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+	
+	TextView textView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView1);
+        
+        new DownloadData().execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
     }
 
 
@@ -32,5 +46,63 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    private class DownloadData extends AsyncTask<String,Void,String> {
+    	
+    	String myXmlData;
+    	
+    	protected String doInBackground(String... urls) {
+    		try {
+    			myXmlData = downloadXML(urls[0]);
+    		} catch(IOException e) {
+    			return "Unable to download XML file.";
+    		}
+    		return "";
+    	}
+    	
+    	protected void onPostExecute(String result) {
+			Log.d("OnPostExecute", myXmlData);
+			textView.setText(myXmlData);
+		}
+    	
+    	private String downloadXML(String theUrl) throws IOException {
+    		int BUFFER_SIZE = 2000;
+    		InputStream is = null;
+    		
+    		String xmlContents = "";
+    		
+    		try {
+    			URL url = new URL(theUrl);
+    			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    			conn.setReadTimeout(10000);
+    			conn.setConnectTimeout(15000);
+    			conn.setRequestMethod("GET");
+    			conn.setDoInput(true);
+    			int response = conn.getResponseCode();
+    			Log.d("DownloadXML", "The response returned is: " + response);
+    			is = conn.getInputStream();
+    			
+    			InputStreamReader isr = new InputStreamReader(is);
+    			int charRead;
+    			char[] inputBuffer = new char[BUFFER_SIZE];
+    			try {
+    				while((charRead = isr.read(inputBuffer))>0) {
+    					String readString = String.copyValueOf(inputBuffer, 0, charRead);
+    					xmlContents += readString;
+    					inputBuffer = new char[BUFFER_SIZE];
+    				}
+    				
+    			} catch(IOException e) {
+    				e.printStackTrace();
+    				return null;
+    			}
+    			
+    			return xmlContents;
+    		} finally {
+    			if (is != null)
+    				is.close();
+    		}
+    	}
     }
 }
